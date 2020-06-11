f7ac3d98b0d2b728c99e3ca039f51eb4b29a4b9a
DewinU <umana.dewin@gmail.com>
2020-06-11 15:16:38 -0600

    Optimizando mainStage

--- src/main/java/Controller/mainStageController.java
+++ src/main/java/Controller/mainStageController.java
@@ -9,6 +9,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class mainStageController {
+    Stage stage;
 
     @FXML
     ComboBox<String> funciones;
@@ -23,13 +24,13 @@ public class mainStageController {
 
     @FXML
     void close(MouseEvent event) {
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.close();
     }
 
     @FXML
     void minus(MouseEvent event) {
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setIconified(true);
     }
 
