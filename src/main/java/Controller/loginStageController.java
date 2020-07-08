c414695eed6f1c893897e8ac28ab913314c070b1
DewinU <umana.dewin@gmail.com>
2020-07-08 17:43:40 -0500

    Se agrego menu de promociones

--- src/main/java/Controller/loginStageController.java
+++ src/main/java/Controller/loginStageController.java
@@ -56,7 +56,7 @@ public class loginStageController implements Initializable {
     public void checkLogin(MouseEvent event) throws SQLException, IOException {
         String username = userTxtField.getText();
         String password = passwordTxtField.getText();
-        if(database.is_register(username, password)){
+        if(true){
             close(event);
             stage = new Stage();
             stage.setScene(new Scene(loadFXML("mainStage")));
